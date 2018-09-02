0fb6e4b7f59be3a04927cbb15990af7203888738
Eugene <eugene.rublenko@gmail.com>
2018-09-02 09:09:03 +0300

    Add vector class

---
+++ dsa/data_structures/vector.hpp
@@ -0,0 +1,247 @@
+#pragma once
+#include <config.hpp>
+
+namespace dsa 
+{
+
+    namespace data_structures 
+    {
+
+        template <typename T>
+        class vector 
+        {
+        public:
+
+            vector(): memory_block(nullptr), capacity(0), length(0) {}
+            
+            vector(config::size_type len): memory_block(new T[len]()), capacity(len), length(len) {}
+            
+            vector(const T& item, int len): vector(len) {
+                for(int i=0, l=length; i<l; ++i)
+                    memory_block[i] = item;
+            }
+
+            vector(std::initializer_list<T> init_list): vector(init_list.size())  {
+                int i = 0;
+                for (const T& e: init_list) {
+                    memory_block[i] = e;
+                    ++i;
+                }
+            }
+
+            vector(const vector &obj): vector(obj.length) {
+                for (int i=0, l=length; i<l; ++i)
+                    memory_block[i] = obj.memory_block[i];
+            }
+            
+            ~vector() {
+                erase();
+            }
+
+            void reserve(config::size_type size) {
+                if(size < 0) 
+                    throw std::length_error("Cannot change capacity to negative value");
+                else if(size <= capacity) 
+                    return;
+
+                capacity = size;
+                T* new_memory_block = new T[capacity];
+
+                for (int i=0, l=length; i<l; ++i)
+                    new_memory_block[i] = memory_block[i];
+
+                delete[] memory_block;
+                memory_block = new_memory_block;
+            }
+
+            void shrink() {
+                capacity = length;
+                T* new_memory_block = new T[capacity];
+
+                for(int i=0, l=length; i<l; ++i)
+                    new_memory_block[i] = memory_block[i];
+
+                delete[] memory_block;
+                memory_block = new_memory_block;
+            }
+
+            T* data() { return &memory_block[0]; }
+            const T* data() const { return &memory_block[0]; }
+
+            T front() const { return memory_block[0]; }
+            T back() const { return memory_block[length-1]; }
+
+            T& operator[](config::index_type index) { return memory_block[index]; }
+            const T& operator[](config::index_type index) const { return memory_block[index]; }
+
+            vector<T>& operator=(const vector &obj) {
+                if(this == &obj)
+                    return *this;
+
+                delete[] memory_block;
+                length = obj.length;
+                capacity = obj.capacity;
+                memory_block = new T[capacity];
+
+                for(int i=0, l=length; i<l; ++i)
+                    memory_block[i] = obj.memory_block[i];
+
+                return *this;
+            }
+
+            bool operator==(const vector &obj) const {
+                if(this == &obj)
+                    return true;
+
+                if(length != obj.length) 
+                    return false;
+
+                for(int i=0, l=length; i<l; ++i)
+                    if(memory_block[i] != obj.memory_block[i]) 
+                        return false;
+
+                return true;
+            }
+
+            bool operator!=(const vector &obj) const {
+                return !(*this == obj);
+            }
+            
+            config::size_type size() const { return length; }
+            config::size_type get_capacity() const { return capacity; }
+
+            bool empty() const {
+                if(length == 0) 
+                    return true;
+                return false;
+            }
+
+            bool contains(const T& item) const {
+                for(int i=0, l=length; i<l; ++i)
+                    if(item == memory_block[i]) return true;
+                return false;
+            }
+
+            int count(T item) const {
+                int amount = 0;
+
+                for(int i = 0, l = length; i < l; ++i)
+                    if(memory_block[i] == item) ++amount;
+
+                return amount;
+            }
+
+            config::index_type find_index_of(const T& item, config::index_type from_index = 0) const {
+                if(from_index >= length || from_index < 0) 
+                    throw std::out_of_range("vector index range was exceeded");
+
+                for(int i = from_index, len = length; i < len; i++)
+                    if(item == memory_block[i]) return i;
+
+                return -1;
+            }
+
+            void reverse() {
+                for(int i=0, len=length; i < (len/2); ++i) {
+                    T temp = memory_block[i];
+                    memory_block[i] = memory_block[len-1-i];
+                    memory_block[len-1-i] = temp;
+                }
+            }
+
+            void sort() {
+                for (int gap = length/2; gap > 0; gap /= 2) {
+                    for (int i = gap, l = length; i < l; i += 1) {
+                        T temp = memory_block[i];
+
+                        int j;
+                        for (j = i; j >= gap && memory_block[j - gap] > temp; j -= gap)
+                            memory_block[j] = memory_block[j - gap];
+
+                        memory_block[j] = temp;
+                    }
+                }
+            }
+
+            void erase() {
+                delete[] memory_block;
+                memory_block = nullptr;
+                length = 0;
+                capacity = 0;
+            }
+
+            void remove_by_index(config::index_type index) {
+                if(length == 0) 
+                    throw std::length_error("vector has zero length");
+                if(index >= length || index < 0) 
+                    throw std::out_of_range("vector index range was exceeded");
+
+                for(int i = index+1, len = length; i < len; ++i)
+                    memory_block[i-1] = memory_block[i];
+                
+                --length;
+            }
+
+            void push_back(const T& item) {
+                if(capacity<length+1)
+                    reserve(length==0 ? 10 : 2*capacity);
+
+                memory_block[length] = item;
+                ++length;
+            }
+
+            T pop_back() {
+                if(length == 0) 
+                    throw std::length_error("vector has zero length");
+                --length;
+                return memory_block[length];
+            }
+
+            T pop_front() {
+                if(length == 0) 
+                    throw std::length_error("vector has zero length");
+
+                T first_elem = memory_block[0];
+                for(int i = 1, l=length; i<l; ++i)
+                    memory_block[i-1] = memory_block[i];
+
+                --length;
+                return first_elem;
+            }
+
+            void insert(config::index_type position, const T& item) {
+                if(position >= length || position < 0) 
+                    throw std::out_of_range("vector index range was exceeded");
+
+                if(capacity<length+1)
+                    reserve(length==0 ? 10 : 2*capacity);
+
+                for(int i = length; i!=position; --i)
+                    memory_block[i] = memory_block[i-1];
+
+                memory_block[position] = item;
+                ++length;
+            }
+
+        private:
+
+            T* memory_block;
+            config::size_type capacity;
+            config::size_type length;
+
+        };
+
+        template<typename T>
+        T* begin(vector<T>& vec)
+        {
+            return vec.data();
+        }
+        template<typename T>
+        T* end(vector<T>& vec)
+        {
+            return vec.data() + vec.size();
+        }
+
+    }
+    
+}
---
+++ tests/data_structures/test_vector.cpp
@@ -0,0 +1,133 @@
+#include <boost/test/unit_test.hpp>
+#include <data_structures/vector.hpp>
+#include <iostream>
+
+using namespace dsa;
+
+BOOST_AUTO_TEST_SUITE( test_vector )
+
+BOOST_AUTO_TEST_CASE( use_case )
+{
+    structs::vector<int> vec_empty;
+    structs::vector<int> vec_def(5);
+    structs::vector<int> vec = {1,2,3};
+    structs::vector<int> vec_copy(vec);
+
+    BOOST_CHECK(vec_empty.empty());
+    BOOST_CHECK(vec_copy == vec);
+
+    for(auto& e: vec_def)
+    	BOOST_CHECK(e == 0);
+    BOOST_CHECK(vec[0] == 1);
+    BOOST_CHECK(vec[1] == 2);
+    BOOST_CHECK(vec[2] == 3);
+    BOOST_CHECK(vec_copy[0] == 1);
+    BOOST_CHECK(vec_copy[1] == 2);
+    BOOST_CHECK(vec_copy[2] == 3);
+
+    BOOST_CHECK(vec_empty.size() == 0);
+    BOOST_CHECK(vec_empty.get_capacity() == 0);
+    BOOST_CHECK(vec_def.size() == 5);
+    BOOST_CHECK(vec_copy.size() == 3);
+    BOOST_CHECK(vec.get_capacity() == 3);
+
+    vec.reserve(5);
+    vec_empty.reserve(5);
+    BOOST_CHECK(vec.get_capacity() == 5);
+    BOOST_CHECK(vec.size() == 3);
+    BOOST_CHECK(vec_empty.get_capacity() == 5);
+    BOOST_CHECK(vec_empty.size() == 0);
+
+    vec.shrink();
+    BOOST_CHECK(vec.get_capacity() == 3);
+    BOOST_CHECK(vec.size() == 3);
+    BOOST_CHECK(vec[0] == 1);
+    BOOST_CHECK(vec[1] == 2);
+    BOOST_CHECK(vec[2] == 3);
+
+    BOOST_CHECK(vec.data() == &vec[0]);
+    BOOST_CHECK(vec.front() == 1);
+    BOOST_CHECK(vec.back() == 3);
+
+    vec_empty = vec_def;
+    for(auto& e: vec_empty)
+    	BOOST_CHECK(e == 0);
+    BOOST_CHECK(vec_empty.size() == 5);
+    BOOST_CHECK(vec_empty == vec_def);
+    BOOST_CHECK(vec_empty != vec);
+    BOOST_CHECK(!vec_empty.empty());
+
+    BOOST_CHECK(vec.contains(1));
+    BOOST_CHECK(vec.contains(2));
+    BOOST_CHECK(vec.contains(3));
+    BOOST_CHECK(!vec.contains(5));
+    BOOST_CHECK(vec_def.contains(0));
+    BOOST_CHECK(!vec_def.contains(1));
+
+    BOOST_CHECK(vec.count(5) == 0);
+    BOOST_CHECK(vec.count(2) == 1);
+    BOOST_CHECK(vec_def.count(0) == 5);
+
+	BOOST_CHECK(vec.find_index_of(1,1) == -1);
+	BOOST_CHECK(vec.find_index_of(1) == 0);
+	BOOST_CHECK(vec.find_index_of(3,1) == 2);
+	BOOST_CHECK(vec.find_index_of(2) == 1);
+	BOOST_CHECK(vec_def.find_index_of(2) == -1);
+	BOOST_CHECK(vec_def.find_index_of(0) == 0);
+	BOOST_CHECK(vec_def.find_index_of(0,2) == 2);
+
+	vec_empty = vec;
+	vec_empty.reverse();
+    BOOST_CHECK(vec_empty[0] == 3);
+    BOOST_CHECK(vec_empty[1] == 2);
+    BOOST_CHECK(vec_empty[2] == 1);
+	vec_empty.sort();
+    BOOST_CHECK(vec_empty[0] == 1);
+    BOOST_CHECK(vec_empty[1] == 2);
+    BOOST_CHECK(vec_empty[2] == 3);
+
+	vec_empty.erase();    
+	BOOST_CHECK(vec_empty.size() == 0);
+
+	vec_copy.remove_by_index(0);
+	BOOST_CHECK(vec_copy.size() == 2);
+	BOOST_CHECK(vec_copy[0] == 2);
+	BOOST_CHECK(vec_copy[1] == 3);
+
+	vec_copy.remove_by_index(1);
+	BOOST_CHECK(vec_copy.size() == 1);
+	BOOST_CHECK(vec_copy[0] == 2);
+
+	vec_copy.push_back(5);
+	BOOST_CHECK(vec_copy.size() == 2);
+	BOOST_CHECK(vec_copy[0] == 2);
+	BOOST_CHECK(vec_copy[1] == 5);
+	vec_copy.push_back(5);
+	BOOST_CHECK(vec_copy.size() == 3);
+	BOOST_CHECK(vec_copy[0] == 2);
+	BOOST_CHECK(vec_copy[1] == 5);
+	BOOST_CHECK(vec_copy[2] == 5);
+
+	vec_empty.push_back(1);
+	BOOST_CHECK(vec_empty[0] == 1);
+	BOOST_CHECK(vec_empty.size() == 1);
+	BOOST_CHECK(vec_empty.get_capacity() > vec_empty.size());
+
+	BOOST_CHECK(vec_copy.pop_back() == 5);
+	BOOST_CHECK(vec_copy.size() == 2);
+	BOOST_CHECK(vec_copy.pop_front() == 2);
+	BOOST_CHECK(vec_copy.size() == 1);
+	
+	// 1 2 3
+	vec.insert(0,0); // 0 1 2 3
+	vec.insert(2,10); // 0 1 10 2 3
+	vec.insert(3,0); // 0 1 10 0 2 3
+    BOOST_CHECK(vec[0] == 0);
+    BOOST_CHECK(vec[1] == 1);
+    BOOST_CHECK(vec[2] == 10);
+    BOOST_CHECK(vec[3] == 0);
+    BOOST_CHECK(vec[4] == 2);
+    BOOST_CHECK(vec[5] == 3);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
