bdc7632535b966168e0f5a3f7c2b52a355077f0b
Eugene <eugene.rublenko@gmail.com>
2018-09-05 10:07:24 +0300

    Improve search algorithms with static_assert

--- dsa/algorithms/search.hpp
+++ dsa/algorithms/search.hpp
@@ -10,21 +10,9 @@ namespace dsa
         namespace detail 
         {
 
-            template <typename InputIterator>
-            inline InputIterator linear_search_impl(InputIterator begin, InputIterator end,
-                                                    const typename std::iterator_traits<InputIterator>::value_type& value,
-                                                    std::input_iterator_tag)
-            {
-                for(; begin != end; ++begin)
-                    if(*begin == value) 
-                    return begin;
-                return end;
-            }
-
             template <typename RandomAccessIterator>
             inline std::pair<bool,RandomAccessIterator> binary_search_impl(RandomAccessIterator begin, RandomAccessIterator end,
-                                                                           const typename std::iterator_traits<RandomAccessIterator>::value_type& value,
-                                                                           std::random_access_iterator_tag)
+                                                                           const typename std::iterator_traits<RandomAccessIterator>::value_type& value)
             {
                 RandomAccessIterator candidate_it = begin + (end-begin)/2;
                 const auto& candidate = *candidate_it;
@@ -33,9 +21,9 @@ namespace dsa
                     return {false,candidate_it};
 
                 if(candidate > value) 
-                    return binary_search_impl(begin, candidate_it, value, std::random_access_iterator_tag{});
+                    return binary_search_impl(begin, candidate_it, value);
                 else if(candidate < value) 
-                    return binary_search_impl(candidate_it, end, value, std::random_access_iterator_tag{});
+                    return binary_search_impl(candidate_it, end, value);
                  else
                     return {true, candidate_it};
             }
@@ -45,13 +33,22 @@ namespace dsa
         template <typename InputIterator>
         InputIterator linear_search(InputIterator begin, InputIterator end, const typename std::iterator_traits<InputIterator>::value_type& value)
         {
-            return detail::linear_search_impl(begin, end, value, typename std::iterator_traits<InputIterator>::iterator_category{});
+            using it_cat = typename std::iterator_traits<InputIterator>::iterator_category;
+            static_assert(std::is_base_of<std::input_iterator_tag, it_cat>::value, "linear_search accepts input iterator");
+
+            for(; begin != end; ++begin)
+                if(*begin == value) 
+                    return begin;
+            return end;
         }
 
         template <typename RandomAccessIterator>
         RandomAccessIterator binary_search(RandomAccessIterator begin, RandomAccessIterator end, const typename std::iterator_traits<RandomAccessIterator>::value_type& value)
         {
-            auto [found,it] = detail::binary_search_impl(begin, end, value, typename std::iterator_traits<RandomAccessIterator>::iterator_category{});
+            using it_cat = typename std::iterator_traits<RandomAccessIterator>::iterator_category;
+            static_assert(std::is_same<it_cat, std::random_access_iterator_tag>::value, "binary_search can accept only random access iterator");
+
+            auto [found,it] = detail::binary_search_impl(begin, end, value);
             return found ? it : end;
         }
 
