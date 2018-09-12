6c4dc617a9117ab5fce5f70728395a62c64ee51b
Eugene <eugene.rublenko@gmail.com>
2018-09-12 08:16:45 +0300

    Add few more sort algorithms

--- dsa/algorithms/sort.hpp
+++ dsa/algorithms/sort.hpp
@@ -76,6 +76,72 @@ namespace dsa
             }
         }
 
+        template <typename I>
+        void binary_insertion_sort(I begin, I end)
+        {
+            using it_cat = typename std::iterator_traits<I>::iterator_category;
+            static_assert(std::is_base_of<std::random_access_iterator_tag, it_cat>::value,
+                         "binary_insertion_sort accepts random access iterator");
+
+            auto it = begin;
+            ++it;
+
+            while(it != end)
+            {
+                auto inner_it = std::upper_bound(begin, it, *it);
+
+                if(inner_it != it)
+                    std::rotate(inner_it,it,++it);
+                else
+                    ++it;
+            }
+        }
+
+        namespace detail
+        {
+            template <typename I>
+            void merge(I begin, I middle, I end)
+            {
+                std::vector<typename I::value_type> sorted;
+                sorted.reserve(std::distance(begin, end));
+
+                auto left_begin = begin;
+                auto right_begin = middle;
+                while(left_begin != middle)
+                {
+                    if(*right_begin < *left_begin && right_begin != end)
+                    {
+                        sorted.emplace_back(*right_begin);
+                        ++right_begin;
+                    } else if(left_begin != middle) {
+                        sorted.emplace_back(*left_begin);
+                        ++left_begin;
+                    }
+                }
+                if(right_begin != end)
+                    sorted.emplace_back(*right_begin);
+
+                std::copy(sorted.begin(), sorted.end(), begin);
+            }
+        }
+        template <typename I>
+        void merge_sort(I begin, I end)
+        {            
+            using it_cat = typename std::iterator_traits<I>::iterator_category;
+            static_assert(std::is_base_of<std::random_access_iterator_tag, it_cat>::value,
+                         "merge_sort accepts random access iterator");
+
+            if(begin+1 == end)
+                return;
+
+            auto middle = begin + std::distance(begin, end) / 2;
+
+            merge_sort(begin, middle);
+            merge_sort(middle, end);
+
+            detail::merge(begin, middle, end);
+        }
+
     }
 
 }
--- tests/algorithms/test_sort.cpp
+++ tests/algorithms/test_sort.cpp
@@ -31,15 +31,21 @@ BOOST_AUTO_TEST_CASE(bubble_sort)
     algos::bubble_sort(dd.begin(), dd.end());
     BOOST_CHECK(std::is_sorted(dd.begin(), dd.end()));
 
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> dist(5,20);
+
     std::vector<int> vi;
     vi.resize(100);
     std::iota(vi.begin(),vi.end(), 1);
     for(int i = 0; i < 100; ++i)
     {
+        auto current_end = vi.end() - dist(rng);
         std::shuffle(vi.begin(), vi.end(), std::default_random_engine{});
+        BOOST_CHECK(!std::is_sorted(vi.begin(), current_end));
+        algos::bubble_sort(vi.begin(), current_end);
+        BOOST_CHECK(std::is_sorted(vi.begin(), current_end));
         BOOST_CHECK(!std::is_sorted(vi.begin(), vi.end()));
-        algos::bubble_sort(vi.begin(), vi.end());
-        BOOST_CHECK(std::is_sorted(vi.begin(), vi.end()));
     }
 }
 
@@ -67,15 +73,21 @@ BOOST_AUTO_TEST_CASE(selection_sort)
     algos::selection_sort(dd.begin(), dd.end());
     BOOST_CHECK(std::is_sorted(dd.begin(), dd.end()));
 
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> dist(5,20);
+
     std::vector<int> vi;
     vi.resize(100);
     std::iota(vi.begin(),vi.end(), 1);
     for(int i = 0; i < 100; ++i)
     {
+        auto current_end = vi.end() - dist(rng);
         std::shuffle(vi.begin(), vi.end(), std::default_random_engine{});
+        BOOST_CHECK(!std::is_sorted(vi.begin(), current_end));
+        algos::selection_sort(vi.begin(), current_end);
+        BOOST_CHECK(std::is_sorted(vi.begin(), current_end));
         BOOST_CHECK(!std::is_sorted(vi.begin(), vi.end()));
-        algos::selection_sort(vi.begin(), vi.end());
-        BOOST_CHECK(std::is_sorted(vi.begin(), vi.end()));
     }
 }
 
@@ -103,15 +115,95 @@ BOOST_AUTO_TEST_CASE(insertion_sort)
     algos::insertion_sort(dd.begin(), dd.end());
     BOOST_CHECK(std::is_sorted(dd.begin(), dd.end()));
 
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> dist(5,20);
+
     std::vector<int> vi;
     vi.resize(100);
     std::iota(vi.begin(),vi.end(), 1);
     for(int i = 0; i < 100; ++i)
     {
+        auto current_end = vi.end() - dist(rng);
         std::shuffle(vi.begin(), vi.end(), std::default_random_engine{});
+        BOOST_CHECK(!std::is_sorted(vi.begin(), current_end));
+        algos::insertion_sort(vi.begin(), current_end);
+        BOOST_CHECK(std::is_sorted(vi.begin(), current_end));
         BOOST_CHECK(!std::is_sorted(vi.begin(), vi.end()));
-        algos::insertion_sort(vi.begin(), vi.end());
-        BOOST_CHECK(std::is_sorted(vi.begin(), vi.end()));
+    }
+}
+
+BOOST_AUTO_TEST_CASE(binary_insertion_sort)
+{
+    std::vector<int> v = {5,4,4,3,2,1};
+    BOOST_CHECK(!std::is_sorted(v.begin(), v.end()));
+    algos::binary_insertion_sort(v.begin(), v.end());
+    BOOST_CHECK(std::is_sorted(v.begin(), v.end()));
+    algos::binary_insertion_sort(v.begin(), v.end());
+    BOOST_CHECK(std::is_sorted(v.begin(), v.end()));
+
+    std::vector<double> d = {3.14};
+    BOOST_CHECK(std::is_sorted(d.begin(), d.end()));
+    algos::binary_insertion_sort(d.begin(), d.end());
+    BOOST_CHECK(std::is_sorted(d.begin(), d.end()));
+
+    std::deque<double> dd = {3.14, -1, 42, 0.0, 0.0, 101, 64.66, 0.0};
+    BOOST_CHECK(!std::is_sorted(dd.begin(), dd.end()));
+    algos::binary_insertion_sort(dd.begin(), dd.end());
+    BOOST_CHECK(std::is_sorted(dd.begin(), dd.end()));
+
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> dist(5,20);
+
+    std::vector<int> vi;
+    vi.resize(100);
+    std::iota(vi.begin(),vi.end(), 1);
+    for(int i = 0; i < 100; ++i)
+    {
+        auto current_end = vi.end() - dist(rng);
+        std::shuffle(vi.begin(), vi.end(), std::default_random_engine{});
+        BOOST_CHECK(!std::is_sorted(vi.begin(), current_end));
+        algos::binary_insertion_sort(vi.begin(), current_end);
+        BOOST_CHECK(std::is_sorted(vi.begin(), current_end));
+        BOOST_CHECK(!std::is_sorted(vi.begin(), vi.end()));
+    }
+}
+
+BOOST_AUTO_TEST_CASE(merge_sort)
+{
+    std::vector<int> v = {5,4,3,2,1};
+    BOOST_CHECK(!std::is_sorted(v.begin(), v.end()));
+    algos::merge_sort(v.begin(), v.end());
+    BOOST_CHECK(std::is_sorted(v.begin(), v.end()));
+    algos::merge_sort(v.begin(), v.end());
+    BOOST_CHECK(std::is_sorted(v.begin(), v.end()));
+
+    std::vector<double> d = {3.14};
+    BOOST_CHECK(std::is_sorted(d.begin(), d.end()));
+    algos::merge_sort(d.begin(), d.end());
+    BOOST_CHECK(std::is_sorted(d.begin(), d.end()));
+
+    std::deque<double> dd = {3.14, -1, 42, 0.0, 0.0, 101, 64.66, 0.0};
+    BOOST_CHECK(!std::is_sorted(dd.begin(), dd.end()));
+    algos::merge_sort(dd.begin(), dd.end());
+    BOOST_CHECK(std::is_sorted(dd.begin(), dd.end()));
+
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> dist(5,20);
+
+    std::vector<int> vi;
+    vi.resize(100);
+    std::iota(vi.begin(),vi.end(), 1);
+    for(int i = 0; i < 100; ++i)
+    {
+        auto current_end = vi.end() - dist(rng);
+        std::shuffle(vi.begin(), vi.end(), std::default_random_engine{});
+        BOOST_CHECK(!std::is_sorted(vi.begin(), current_end));
+        algos::merge_sort(vi.begin(), current_end);
+        BOOST_CHECK(std::is_sorted(vi.begin(), current_end));
+        BOOST_CHECK(!std::is_sorted(vi.begin(), vi.end()));
     }
 }
 
